e1e4f33ff59fa21e05d976a7c424d8c05c8a2c30
Alex Kim <alexkimt@gmail.com>
2020-02-20 14:54:20 -0500

    Added score method to library Started implementation of greedy method of score/setupTime

--- src/Library.java
+++ src/Library.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Set;
 
 public class Library {
 
     public ArrayList<Book> books;
-    public int signUp;
+    public int signUpTime;
     public int booksPerDay;
 
-    public Library(int signUp, int booksPerDay) {
-        this.signUp = signUp;
+    public Library(int signUpTime, int booksPerDay) {
+        this.signUpTime = signUpTime;
         this.booksPerDay = booksPerDay;
         books = new ArrayList();
     }
@@ -18,15 +19,33 @@ public class Library {
 
     public void sort() {
         Collections.sort(books);
+        Collections.reverse(books);
     }
 
     public String toString() {
         String returnS = "";
-        returnS += ("SignUp: " + signUp + "\n");
+        returnS += ("SignUp: " + signUpTime + "\n");
         returnS += ("BooksPerDay: " + booksPerDay + "\n");
         for(Book book: books) {
             returnS += (book + " ");
         }
         return returnS;
+    }
+
+    public int score(int daysLeft, Set<Integer> scannedBooks) {
+        daysLeft -= signUpTime;
+        int score = 0;
+        for (int day = 0; day < daysLeft; day++) {
+            int book = 0;
+            for (int i = 0; i < booksPerDay; i++) {
+                if (!scannedBooks.contains(books.get(i))) {
+                    score += books.get(book).getScore();
+                } else {
+                    i--; //since we skipped a book
+                }
+                book++;
+            }
+        }
+        return score;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
-public class Main {
+import java.util.*;
 
+public class Main {
+    static int totalDays;
+    static Book[] allBooks;
+    static ArrayList<Library> allLibraries;
+    static Set<Integer> scannedBooks;
+    static ArrayList<Library> librarySchedule;
     public static void main(String[] args) throws Exception {
         String file = "e_so_many_books.txt";
 
         Object[] data = Parser.readFile(file);
 
-        int days = (int) data[0];
-        Book[] allBooks = (Book[]) data[1];
-        Library[] allLibraries = (Library[]) data[2];
+        totalDays = (int) data[0];
+        allBooks = (Book[]) data[1];
+        Library[] arrLibs = (Library[]) data[2];
+        allLibraries = new ArrayList<>(Arrays.asList(arrLibs));
+        Set<Integer> scannedBooks = new HashSet();
+        librarySchedule = maxScoreOverSetUp(totalDays);
+    }
+
+    public static ArrayList<Library> maxScoreOverSetUp (int daysLeft) {
+        ArrayList<Library> schedule = new ArrayList<>();
+        //maxVal is score/setuptime
+        int maxVal = allLibraries.get(0).score(daysLeft, scannedBooks) / allLibraries.get(0).signUpTime;
+        Library maxLib = allLibraries.get(0);
+        for (Library lib : allLibraries) {
+            int libVal = lib.score(daysLeft, scannedBooks) / lib.signUpTime;
+            if (libVal > maxVal) {
+                maxLib = lib;
+                maxVal = libVal;
+            }
+        }
+        //we choose maxLib to add to our schedule
+        schedule.add(maxLib);
+        allLibraries.remove(maxLib);
+        return null;
+
+
 
     }
 }
