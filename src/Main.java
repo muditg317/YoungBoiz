500cc550f907f855c6c84fef516647dc4409c2c8
Mudit Gupta <muditg317@gmail.com>
2020-02-20 14:38:23 -0500

    use res folder

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String file = "e_so_many_books.txt";
+        String file = "src/res/a_example.txt";
 
         Object[] data = Parser.readFile(file);
 
         int days = (int) data[0];
         Book[] allBooks = (Book[]) data[1];
         Library[] allLibraries = (Library[]) data[2];
-
+        System.out.println(Arrays.toString(allLibraries));
     }
 }
